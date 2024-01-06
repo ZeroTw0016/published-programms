e3594b29a4c7328d2c068b91478260355a4acb80
ZeroTw0016 <84594740+ZeroTw0016@users.noreply.github.com>
2024-01-06 05:02:26 +0100

    source code for update.ps1
    Signed-off-by: ZeroTw0016 <84594740+ZeroTw0016@users.noreply.github.com>

---
+++ lethal-company/update.ps1
@@ -0,0 +1,4 @@
+﻿cd $env:ProgramData\ZeroModded
+Remove-Item -Path .\Zeros-LethalModder.exe -Force
+Move-Item -Path .\Zeros-LethalModder_New.exe -Destination .\Zeros-LethalModder.exe -Force
+.\Zeros-LethalModder.exe
