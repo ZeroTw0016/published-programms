e00738c475a765e89aa0bc21bd5bcffc7375f73d
ZeroTwo <thezerotwo016@gmail.com>
2024-01-27 02:36:44 +0100

    try fix

---
+++ lethal-company/updater.ps1
@@ -0,0 +1,13 @@
+﻿Sleep(1)
+Set-Location -Path "C:\ProgramData\ZeroModded"
+if(Get-Process | Where-Object {$_.ProcessName -eq "Zeros-LethalModder"}) {
+    Get-Process | Where-Object {$_.ProcessName -eq "Zeros-LethalModder"} | Stop-Process -Force
+    while(Get-Process | Where-Object {$_.ProcessName -eq "Zeros-LethalModder"}) {
+        Sleep(1)
+    }
+}
+if(Test-Path -Path .\Zeros-LethalModder.exe) {
+    Remove-Item -Path .\Zeros-LethalModder.exe -Force
+}
+Invoke-WebRequest "https://github.com/ZeroTw0016/published-programms/raw/main/lethal-company/Zeros-LethalModder.exe" -OutFile Zeros-LethalModder.exe
+.\Zeros-LethalModder.exe
